97fe734f17183324ee737b7c51d247db89ef88ac
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-27 08:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,15 +41,14 @@ function total() {
 }
 
 function removeFromCart(item) {
-  if (item != cart[item]) {
-    return 'That item is not in your cart.'
-  } else {
-    for (let i = 0; i < cart.length; i++) {
-      if (item === cart[i].itemName) {
-        cart.splice(i, 1)
-        return cart
-      }
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1); //removes 1 item at index i
+      return cart;
     }
+  }
+  if (item != cart[item]) {
+    return "That item is not in your cart."
   }
 }
 
