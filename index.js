aba95d05efbc89022b9ec06997c79d2a109cb840
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-27 08:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function placeOrder(cardNumber) {
   if (typeof(cardNumber) === "undefined"){
   return "Sorry, we don't have a credit card on file for you."
   } else {
-    cart = [];
+    cart = []
+    placeOrder(cardNumber)
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
