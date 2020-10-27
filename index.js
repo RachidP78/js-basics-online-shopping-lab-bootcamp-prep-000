6aa575d1ad0dd18bd417df47444d6a08a211020e
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-27 07:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,17 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return ('Your shopping cart is empty.')
+  } else if (cart.length === 1) {
+    return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`)
+  } else if (cart.length === 2) {
+    return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`)
   } else {
-    var itemsList = []
-    for (let i = 0; i < cart.length; i++) {
-      itemsList.push(cart[i].caritemObj.key)
+    var itemsList = "In your cart, you have"
+    for (let i = 0; i < cart.length - 1; i++) {
+      itemsList = itemsList + `${cart[i].itemName} at $${cart[i].itemPrice},`
     }
-    return ('In your cart, you have' + itemsList)
+    return itemsList + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
   }
-  // write your code here
 }
 
 function total() {
