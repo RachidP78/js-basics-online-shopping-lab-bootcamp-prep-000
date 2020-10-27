e55b36c5508014f3faa98ca64ffabc812ed03f64
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-27 07:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor(Math.random() * 100) + 1
   var caritemObj = {itemName: `${item}`, itemPrice}
-  //var price = Math.floor(Math.random() * 100) + 1
   cart.push(caritemObj)
   return (`${caritemObj.itemName} has been added to your cart.`)
 }
 
 function viewCart() {
+  if (cart.length === 0) {
+    return ('Your shopping cart is empty.')
+  } else {
+    var itemsList = []
+    for (let i = 0; i < cart.length; i++) {
+      itemsList.push(cart[i].caritemObj.key)
+    }
+    return ('In your cart, you have' + itemsList)
+  }
   // write your code here
 }
 
