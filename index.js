63cbb833d0281ae0e68a2895f385354e56e56005
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-27 07:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
 function total() {
   var total = 0
   for (let i = 0; i > cart.length; i++) {
-    total += parceInt(cart[i].itemPrice)
+    total += parseInt(cart[i].itemPrice)
   }
   return total
   // write your code here
